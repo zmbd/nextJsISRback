e2f91acca253f53c9ed8c8e1d651a5476a9d156d
Matas Cypas <mcypaszh@gmail.com>
2023-07-27 11:42:46 +0300

    asdada

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 4848;
+
+const clients = [
+  {
+    client: "morningstar",
+    content: "Yes, hello, I'm Morningstar",
+  },
+  {
+    client: "apollo",
+    content: "yesyesyeyseyseyse",
+  },
+  {
+    client: "crazy8",
+    content: "crazy 8 DDDDDDDDDD",
+  },
+];
+
+app.get("/clients", (req, res) => {
+  res.json(clients);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
